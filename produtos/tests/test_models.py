9aea243a858cfd5ea3ae0ece8aaeaa5f61ec1cbf
Leonardo Teixeira <leovenancio00@gmail.com>
2019-10-02 21:12:40 -0300

    Primeiro Commit

---
+++ produtos/tests/test_models.py
@@ -0,0 +1,16 @@
+from mixer.backend.django import mixer
+
+import pytest
+
+@pytest.mark.django_db
+
+class TestModels:
+    
+    def test_produto_em_esqtoque(self):
+        produto = mixer.blend(
+            'produtos.Produto',
+            quantidade=1
+        )
+
+        assert produto.tem_no_estoque == True
+    
